10e29f32e881ca88ee7f7f766f4fd3c14a93dfcf
Ferry Chandra <ignatiusferry5245@gmail.com>
2023-10-06 21:48:57 +0700

    Update App File

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
 
 export default function App() {
   return (
     <>
-    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    <Footer/>
     </>
   );
 }
