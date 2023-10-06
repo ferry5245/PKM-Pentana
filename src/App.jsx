2648c6f2f47c86fae0464917bd9122f4a3b3ba41
Ferry Chandra <ignatiusferry5245@gmail.com>
2023-10-06 22:50:01 +0700

    Create First Version of Tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Tracking from "./Pages/Tracking/Tracking";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/tracking" element={<Tracking />} />
       </Routes>
     </>
   );
